fa0d97d402dff4f8310f2e934f983258026e3ef2
CI <CI@example.com>
2021-06-28 10:25:28 +0000

    deploy

---
+++ precache-manifest.07aa2eda92c91029639c19ed3ad140ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78eb601f85c51fd1e09f",
+    "url": "/Cashier-Register-App/css/app.c4278f23.css"
+  },
+  {
+    "revision": "dfa635107a4d5c4484b23431f68d8645",
+    "url": "/Cashier-Register-App/index.html"
+  },
+  {
+    "revision": "78eb601f85c51fd1e09f",
+    "url": "/Cashier-Register-App/js/app.30996913.js"
+  },
+  {
+    "revision": "6f87d4fa2c63b5d73e9a",
+    "url": "/Cashier-Register-App/js/chunk-vendors.7320ab10.js"
+  },
+  {
+    "revision": "f068212d03be46e717d1db3b480db8c9",
+    "url": "/Cashier-Register-App/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Cashier-Register-App/robots.txt"
+  }
+]);
